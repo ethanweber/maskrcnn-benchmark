495a074d9e0f59595b7ae6055d6929a7682e5512
rishisun@mit.edu <rishisun@mit.edu>
2018-11-02 01:21:34 -0400

    Added file to rotate around pivot

---
+++ demo/rotate_around_pivot.py
@@ -0,0 +1,28 @@
+import math
+import numpy as np
+
+def rotate_around_pivot(pixels, pivot, theta):
+	"""pixels: dictionary: (x,y): pixel value
+   pivot: pivot (a,b) to rotate around"""
+	moved_pixels = dict()
+	for point in pixels:
+		moved_pixels[rotate_point_around_pivot(point, pivot, theta)] = pixels[point]
+	return moved_pixels
+		
+
+
+def rotate_point_around_pivot(point, pivot, theta):
+	"""rotates point counterclockwise around pivot by angle theta"""
+	theta = math.radians(theta)
+
+	# 1) subtract pivot from point
+	(a,b) = point[0] - pivot[0], point[1] - pivot[1]
+
+	#2) rotate temp_point about origin by theta
+	(a,b) = (a*np.cos(theta)+b*np.sin(theta), a*np.sin(theta)+b*np.cos(theta))
+
+	#3) add back the pivot for final point
+	final_point = int(a+pivot[0]+0.5), int(b+pivot[1]+0.5)
+
+	return final_point
+

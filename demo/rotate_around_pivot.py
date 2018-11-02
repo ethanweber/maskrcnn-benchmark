8045bd3fc5dc9bb07f281a0ae370ff153d3769f8
rishisun@mit.edu <rishisun@mit.edu>
2018-11-02 01:57:48 -0400

    fixed bug

--- demo/rotate_around_pivot.py
+++ demo/rotate_around_pivot.py
@@ -12,17 +12,16 @@ def rotate_around_pivot(pixels, pivot, theta):
 
 
 def rotate_point_around_pivot(point, pivot, theta):
-	"""rotates point counterclockwise around pivot by angle theta"""
+	"""rotates point clockwise around pivot by angle theta"""
 	theta = math.radians(theta)
 
 	# 1) subtract pivot from point
 	(a,b) = point[0] - pivot[0], point[1] - pivot[1]
-
 	#2) rotate temp_point about origin by theta
-	(a,b) = (a*np.cos(theta)+b*np.sin(theta), a*np.sin(theta)+b*np.cos(theta))
+	(a,b) = (a*np.cos(theta)-b*np.sin(theta), a*np.sin(theta)+b*np.cos(theta))
+	# print((a,b))
 
 	#3) add back the pivot for final point
 	final_point = int(a+pivot[0]+0.5), int(b+pivot[1]+0.5)
 
 	return final_point
-

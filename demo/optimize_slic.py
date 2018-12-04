d8ec648fbbb21e27dbd63d9d691877b702d3908f
rishisun@mit.edu <rishisun@mit.edu>
2018-12-03 19:38:05 -0500

    images can be passed in

--- demo/optimize_slic.py
+++ demo/optimize_slic.py
@@ -1,48 +1,134 @@
 from random import sample
+from random import randint
 import numpy as np
+from scipy import stats
+from skimage import io
+import matplotlib.pyplot as plt
+
+from superpixel_segmentation_SLIC import MaskEnhancer
 
 class OptimizeSLIC:
 
-	def __init__(self, truths, masks, images, num_training_points):
+	def __init__(self, truths=None, masks=None, images=None, num_training_points=1):
 		self.truths = truths
 		self.masks = masks
 		self.images = images
 		self.n = num_training_points
 
-	def optimize(self):
-		#returns best resolution
+	def optimize(self, max_points = 500):
+
+		#Similar to Peak Search Algorithm
 		best_resolution = 0
 		best_loss = float('inf')
-		for resolution in range(1,1001):
-			average_loss = self.evaluate(resolution)
-			if average_loss < best_loss:
-				best_loss = average_loss
-				best_resolution = resolution
-		return resolution
+		data = []
 
-	def evaluate(self, resolution, batch_size=10):
+		resolution_max = 1000
+		max_learning_rate = 100
+		resA = int(resolution_max*0.3)
+		previous_resolution = resA
+		previous_loss = self.evaluate(resA)
+
+		resB = int(resolution_max*0.7)
+		second_previous_resolution = resB
+		second_previous_loss = self.evaluate(resB)
+		resolutions_sampled = 0
+		while resolutions_sampled < max_points:
+
+			case_increase = second_previous_resolution < previous_resolution and second_previous_loss > previous_loss
+			case_increase = case_increase or (second_previous_resolution > previous_resolution and second_previous_loss < previous_loss)
+
+			case_decrease = second_previous_resolution < previous_resolution and second_previous_loss < previous_loss
+			case_decrease = case_decrease or (second_previous_resolution > previous_resolution and second_previous_loss > previous_loss)
+
+			new_resolution = max(min(previous_resolution + randint(-20,20), resolution_max), 5)
+			if case_increase:
+				new_resolution = min(previous_resolution + randint(1,max_learning_rate), resolution_max)
+			if case_decrease:
+				new_resolution = max(previous_resolution - randint(1,max_learning_rate), 20)
+
+			new_avg_loss = self.evaluate(new_resolution)
+			data.append((new_resolution, new_avg_loss))
+			if new_avg_loss < best_loss:
+				best_loss = new_avg_loss
+				best_resolution = new_resolution
+
+			second_previous_resolution = previous_resolution
+			second_previous_loss = previous_loss
+			previous_resolution = new_resolution
+			previous_loss = new_avg_loss
+			print("Resolution: " + str(new_resolution) + " Loss: " + str(new_avg_loss) +  "  Count: " + str(resolutions_sampled))
+			resolutions_sampled += 1
+			if resolutions_sampled % 50 == 0:
+				print("Best Resolution: " + str(best_resolution))
+				print("Best Loss: " + str(best_loss))
+		return data, best_resolution, best_loss
+
+	def evaluate(self, resolution, batch_size=100):
 		points = sample(range(0,self.n-1), batch_size)
-		loss = np.mean([self.calculate_loss(truths[i], masks[i], images[i], resolution) for i in points])
-		return loss
+		losses = [self.calculate_loss(truths[i], masks[i], images[i], resolution) for i in points]
+		losses = [loss for loss in losses if loss >= 0]
+		return np.mean(losses)
 
-	def calculate_loss(truth, mask, image, resolution):
-		total_pixels = numpy.prod(image.size())/3.0
-		enhancer = MaskEnhancer(image, mask, resolution)
+	def calculate_loss(self, truth_string, mask_string, image_string, resolution):
+		truth = io.imread(truth_string)[:,:,0]
+		total_pixels = truth.size
+
+
+		enhancer = MaskEnhancer(io.imread(image_string), io.imread(mask_string), resolution, string=False)
 		enhancer.get_SLIC_segments(show_segments=False)
-		enhanced_mask = enhancer.tune_mask_expand()
+		enhanced_mask, original_mask_pixels = enhancer.tune_mask_expand(show_masks=False)
+		if len(original_mask_pixels) == 0:
+			return -1 #ERROR
 
+		#get ground truth mask pixel value to look for
+		mask_value = self.find_ground_truth_mask_value(truth, original_mask_pixels)
+
+		#Calculate missing and extra pixels
 		missed_pixels = 0
 		extra_pixels = 0
-		for r in range(len(mask)):
-			for c in range(len(mask[0])):
-				if truth[r][c] > 0 and mask[r][c] == 0:
+		for r in range(len(enhanced_mask)):
+			for c in range(len(enhanced_mask[0])):
+				if truth[r][c] == mask_value and enhanced_mask[r][c] == 0:
 					missed_pixels += 1
-				if truth[r][c] == 0 and mask[r][c] > 0:
+				if truth[r][c] != mask_value and enhanced_mask[r][c] > 0:
 					extra_pixels += 1
+		#calculate loss
+		loss = missed_pixels + extra_pixels*1.0/total_pixels
+		return loss
 
-		return total_pixels*missed_pixels + extra_pixels
-	
+	def find_ground_truth_mask_value(self,truth, mask_pixels):
+		locations = sample(mask_pixels, min(100, len(mask_pixels)-1))
+		values = [truth[r][c] for r,c in locations]
+		value = stats.mode(values)[0][0]
+		return value
 
+	def plot(self, data):
+		data2 = sorted(data, key = lambda x: x[0])
+		resolutions, losses = zip(*data2)
+
+		plt.figure()
+		plt.plot(resolutions, losses)
+		plt.show()
+
+import glob
+images = sorted(glob.glob("../2000_validation_cropped_dataset_time551/rgb_crop/*.jpg"))
+truths = sorted(glob.glob("../2000_validation_cropped_dataset_time551/gt_mask/*.jpg"))
+masks = sorted(glob.glob("../2000_validation_cropped_dataset_time551/predicted_mask/*.jpg"))
+
+
+optimizer = OptimizeSLIC(truths = truths, masks=masks, images = images, num_training_points=len(images))
+data, best_resolution, best_loss = optimizer.optimize()
+optimizer.plot(data)
+print("Best Resolution: " + str(best_resolution))
+print("Best Loss: " + str(best_loss))
+
+
+# mask = "crop_mask.jpg"
+# image = "crop.jpg"
+# truth = "actual_mask.jpg"
+# loss = optimizer.calculate_loss(truth, mask, image)
+# print("LOSS")
+# print(loss)
 
 
 
--- demo/superpixel_segmentation_SLIC.py
+++ demo/superpixel_segmentation_SLIC.py
@@ -12,52 +12,62 @@ from skimage import io
 
 
 class MaskEnhancer:
-	def __init__(self, image_string, mask_string=None, resolution = 250):
-		raw_img = io.imread(image_string)
-		self.image = img_as_float(raw_img)
-		self.mask = io.imread(mask_string)
-		self.segments_slic = []
-		self.n_segments = resolution
+	def __init__(self, image, mask=None, resolution = 250, string=True):
+		if string:
+			raw_img = io.imread(image)
+			self.original_image = img_as_float(raw_img)
+			self.image = img_as_float(raw_img)[:,:,0]
+			self.mask = io.imread(mask)[:,:,0]
+			self.segments_slic = []
+			self.n_segments = resolution
+		else:
+			raw_img = image
+			self.original_image = img_as_float(raw_img)
+			self.image = img_as_float(raw_img)[:,:,0]
+			self.mask = mask[:,:,0]
+			self.segments_slic = []
+			self.n_segments = resolution
 
 	def get_SLIC_segments(self, show_segments=True):
-		print("Getting Segments...")
+
 		segments_slic = slic(self.image, self.n_segments, compactness=10, sigma=1)
 
 		if self.n_segments < 5000 and show_segments:
-			print('SLIC number of segments: {}'.format(len(np.unique(segments_slic))))
 			fig, ax = plt.subplots(1, 1, figsize=(10, 10), sharex=True, sharey=True)
-
 			ax.imshow(mark_boundaries(self.image, segments_slic))
 			ax.set_title('SLIC')
 			plt.show()
 		self.segments_slic = segments_slic
 		return segments_slic
 
-	def tune_mask_expand(self):
+	def tune_mask_expand(self, show_masks=True):
 		""" Expands mask according to superpixel segmentation"""
-		mask_pixels = list(zip(*np.where(self.mask == 100)))
-		imgplot = plt.imshow(self.mask)
-		plt.title("Before Enhancement")
-		plt.show()
-
+		mask_pixels = list(zip(*np.where(self.mask > 0)))
+		if show_masks:
+			imgplot = plt.imshow(self.original_image)
+			plt.title("Image")
+			plt.show()
+			imgplot = plt.imshow(self.mask)
+			plt.title("Before Enhancement")
+			plt.show()
+		
 		#find segments with parts in mask
-		print("Finding Relevant Segments...")
 		present_segments = set()
 		for r,c in mask_pixels:
 			if self.segments_slic[r][c] not in present_segments:
 				present_segments.add(self.segments_slic[r][c])
 
 		#fill in the mask with those segments
-		print("Updating Mask...")
 		for r in range(len(self.segments_slic)):
 			for c in range(len(self.segments_slic[0])):
 				if self.segments_slic[r][c] in present_segments:
 					self.mask[r][c] = 100
-
-		imgplot = plt.imshow(self.mask)
-		plt.title("After Enhancement")
-		plt.show()
-		return self.mask
+					
+		if show_masks:
+			imgplot = plt.imshow(self.mask)
+			plt.title("After Enhancement")
+			plt.show()
+		return self.mask, mask_pixels
 
 	def tune_mask_trim(self):
 		""" Trims mask according to superpixel segmentation"""
@@ -86,6 +96,6 @@ class MaskEnhancer:
 		plt.show()
 		return self.mask
 
-# enhancer = MaskEnhancer('demo_images/caterpillar.png', 'demo_images/mask.png', resolution=5e2)
+# enhancer = MaskEnhancer(io.imread('demo_images/caterpillar.png'), io.imread('demo_images/mask.png'), resolution=5e2, string=False)
 # enhancer.get_SLIC_segments()
 # enhancer.tune_mask_expand()
